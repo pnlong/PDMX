dc43b15aada5d3d1e4295f09f4116ce513d04cf7
pnlong <phillipnlong@gmail.com>
2024-01-04 14:08:54 -0800

    added train.sh

---
+++ train.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# README
+# Phillip Long
+# January 4, 2023
+
+# A place to store the command-line inputs for different models to train.
+
+# sh /home/pnlong/model_musescore/train.sh
+
+python /home/pnlong/model_musescore/train.py
