4e4eb22fcd300c0d6a38202beb82e4c9ad1cf756
pnlong <phillipnlong@gmail.com>
2024-08-04 16:23:30 -0700

    moved files around

---
+++ old_scripts/create_expression_net.py
@@ -0,0 +1,247 @@
+# README
+# Phillip Long
+# March 28, 2024
+
+# Make the main dataset for the paper.
+
+# python /home/pnlong/model_musescore/create_expression_net.py
+
+
+# IMPORTS
+##################################################
+
+from os.path import exists, basename, dirname
+from os import makedirs, mkdir
+import subprocess
+from shutil import copy
+import pandas as pd
+from tqdm import tqdm
+from time import perf_counter, strftime, gmtime
+import multiprocessing
+import argparse
+import logging
+from read_mscz.read_mscz import read_musescore
+from train import NA_VALUE
+import json
+
+##################################################
+
+
+# CONSTANTS
+##################################################
+
+INPUT_DIR = "/data2/pnlong/musescore/expressive_features"
+OUTPUT_DIR = "/data2/pnlong/musescore"
+
+DATASET_NAME = "ExpressionNet"
+
+OUTPUT_COLUMNS = ["path", "metadata", "genres", "tags", "n_tracks", "n_expressive_features", "n_tokens"]
+
+##################################################
+
+
+# ARGUMENTS
+##################################################
+
+def parse_args(args = None, namespace = None):
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser(prog = "Parse MuseScore", description = "Extract expressive features from MuseScore files.")
+    parser.add_argument("-i", "--input_dir", type = str, default = INPUT_DIR, help = "Output directory provided to `parse_mscz.py`")
+    parser.add_argument("-o", "--output_dir", type = str, default = OUTPUT_DIR, help = "Output directory where the CSV mappings file and the directory with the actual dataset will be stored")
+    parser.add_argument("-n", "--nested", action = "store_true", help = "Whether to replicate Herman's nested directory structure")
+    parser.add_argument("-m", "--metadata", action = "store_true", help = "Whether to store metadata paths in the resulting CSV file")
+    parser.add_argument("-j", "--jobs", type = int, default = int(multiprocessing.cpu_count() / 4), help = "Number of Jobs")
+    return parser.parse_args(args = args, namespace = namespace)
+
+##################################################
+
+
+# FUNCTION TO PRODUCE JSON FILES FOR DATASET
+##################################################
+
+# main function for getting data
+def get_data(input_path: str, output_path: str, compressed: bool = False) -> str:
+    """
+    Given the input path, load that MuseScore file and save it in JSON format.
+    If the file loads incorrectly, return None; otherwise, return the `output_path`.
+
+    Parameters
+    ----------
+    input_path : str
+        Path to the MuseScore file to read.
+    output_path : str
+        Path where the MusicExpress JSON file will be saved.
+    compressed : bool
+        Whether or not to compress the JSON output.
+
+    Returns
+    -------
+    :str:
+        The path to which the MusicExpress JSON file was successfully saved. If the file loads incorrectly, returns None.
+    
+    """
+
+    # avoid doing if we don't need to
+    if exists(output_path):
+        return output_path
+
+    # try to read in musescore object
+    try:
+        music = read_musescore(path = input_path, timeout = 10)
+        music.save_json(path = output_path, compressed = compressed)
+        return output_path
+    
+    # if that fails, return None
+    except:
+        return None
+    
+
+# function to help with copying metadata files
+def get_metadata(input_path: str) -> str:
+    """Copy over a metadata file, returning the copied filepath."""
+    output_dir = f"{METADATA_DIR}" + (f"/{basename(dirname(input_path))}" if args.nested else "")
+    if not exists(output_dir):
+        try:
+            mkdir(output_dir)
+        except (FileExistsError):
+            pass
+    output_path = f"{output_dir}/{basename(input_path)}" # get output filepath
+    copy(src = input_path, dst = output_path) # copy over file
+    return output_path
+
+##################################################
+
+
+# for multiprocessing, must use a main class
+if __name__ == "__main__":
+
+    # ARGS AND CONSTANTS
+    ##################################################
+
+    args = parse_args()
+    if not exists(args.output_dir): # make output_dir if it doesn't yet exist
+        makedirs(args.output_dir)
+
+    # output filepaths and directory
+    OUTPUT_DIR_MAIN = f"{args.output_dir}/{DATASET_NAME}"
+    if not exists(OUTPUT_DIR_MAIN): # make output_dir if it doesn't yet exist
+        mkdir(OUTPUT_DIR_MAIN)
+    OUTPUT_PATH = f"{OUTPUT_DIR_MAIN}/{DATASET_NAME}.csv"
+    DATA_DIR = f"{OUTPUT_DIR_MAIN}/data"
+    if not exists(DATA_DIR):
+        mkdir(DATA_DIR)
+        if args.nested:
+            subprocess.run(args = ["bash", f"{dirname(__file__)}/create_data_dir.sh", "-d", DATA_DIR], check = True)
+    METADATA_DIR = f"{OUTPUT_DIR_MAIN}/metadata"
+    if args.metadata and not exists(METADATA_DIR):
+        mkdir(METADATA_DIR)
+
+    # load in data frame
+    dataset = pd.read_csv(filepath_or_buffer = f"{args.input_dir}/{basename(args.input_dir)}.path.csv", sep = ",", header = 0, index_col = False)
+    dataset = dataset[dataset["in_dataset"]].drop(columns = "in_dataset") # make sure we only have relevant files
+    dataset = dataset[OUTPUT_COLUMNS] # get the right columns
+
+    # set up logging
+    logging.basicConfig(level = logging.INFO, format = "%(message)s")
+
+    ##################################################
+
+
+    # GET LIST OF FILES
+    ##################################################
+
+    # use glob to get all mscz files
+    input_paths = dataset["path"].tolist()
+
+    # get output filepaths
+    if args.nested:
+        get_output_path = lambda path: f"{DATA_DIR}/{('/'.join(path.split('/')[-3:])).split('.')[0]}.json"
+    else:
+        get_output_path = lambda path: f"{DATA_DIR}/{basename(path).split('.')[0]}.json"
+    output_paths = tuple(map(get_output_path, input_paths))
+
+    ##################################################
+
+
+    # MAKE DATASET
+    ##################################################
+
+    # some pre dataset building stuff
+    logging.info(f"N_PATHS = {len(input_paths)}") # print number of paths to process
+    chunk_size = 1
+    make_path_relative = lambda path: path.replace(OUTPUT_DIR_MAIN, ".")
+    start_time = perf_counter() # start the timer
+
+    # use multiprocessing to generate dataset
+    with multiprocessing.Pool(processes = args.jobs) as pool:
+
+        # get json files
+        output_paths = pool.starmap(
+            func = get_data,
+            iterable = tqdm(
+                iterable = zip(input_paths, output_paths),
+                desc = f"Creating {DATASET_NAME}",
+                total = len(input_paths)),
+            chunksize = chunk_size)
+        dataset["path"] = output_paths
+        dataset = dataset[~pd.isna(dataset["path"])]
+        dataset["path"] = dataset["path"].apply(make_path_relative) # transform output paths into relative paths
+        
+        # copy metadata files over, or drop the metadata column altogether
+        if args.metadata:
+            valid_metadata_paths = ((~pd.isna(dataset["metadata"])) & dataset["metadata"].apply(exists))
+            dataset.loc[~valid_metadata_paths, "metadata"] = None
+            metadata_paths = list(tqdm(
+                iterable = pool.imap(
+                    func = get_metadata,
+                    iterable = dataset["metadata"][valid_metadata_paths],
+                    chunksize = chunk_size
+                ),
+                desc = "Copying over metadata",
+                total = sum(valid_metadata_paths)))
+            dataset.loc[valid_metadata_paths, "metadata"] = list(map(make_path_relative, metadata_paths))
+        else:
+            dataset = dataset.drop(columns = "metadata")    
+    
+    # output total time
+    end_time = perf_counter() # stop the timer
+    total_time = end_time - start_time # compute total time elapsed
+    total_time = strftime("%H:%M:%S", gmtime(total_time)) # convert into pretty string
+    logging.info(f"Total time: {total_time}")    
+
+    # save dataset mapping
+    dataset.to_csv(path_or_buf = OUTPUT_PATH, sep = ",", na_rep = NA_VALUE, header = True, index = False, mode = "w")
+
+    ##################################################
+
+
+# CHECK PUBLIC DOMAIN
+##################################################
+
+def check_public_domain() -> pd.DataFrame:
+
+    # load in dataset
+    dataset_dir = f"{OUTPUT_DIR}/{DATASET_NAME}"
+    data = pd.read_csv(filepath_or_buffer = f"{dataset_dir}/{DATASET_NAME}.csv", sep = ",", header = 0, index_col = False)
+    get_absolute_path = lambda path: None if (path is None) else f"{dataset_dir}{path[1:]}" # path starts with a ".", replace with the full directory
+    data["path"] = list(map(get_absolute_path, data["path"]))
+    data["metadata"] = list(map(get_absolute_path, data["metadata"]))
+
+    # extract license strings from metadata filepath
+    def extract(metadata_path: str) -> str:
+        with open(metadata_path, "r") as metadata_file:
+            metadata = json.load(fp = metadata_file)
+        return metadata["data"].get("license_string", "")
+    license_strings = pd.Series(list(map(extract, data["metadata"])))
+
+    # extract info
+    licenses = pd.DataFrame(license_strings.value_counts())
+    licenses["percent"] = 100 * (licenses["count"] / sum(licenses["count"]))
+    licenses["name"] = list(map(lambda license: license[(license.rindex("</i>") + 5):].replace("</a>", "").strip(), licenses.index))
+    licenses["link"] = list(map(lambda license: license[(license.index("href") + 6):(license.index("target") - 2)] if ("href" in license) else None, licenses.index))
+    licenses = licenses[["name", "link", "count", "percent"]]
+
+    # return the dataframe
+    return licenses
+
+##################################################
--- remap_metadata_to_data.py
+++ remap_metadata_to_data.py
@@ -2,12 +2,13 @@
 # Phillip Long
 # September 26, 2023
 
-# correct the filepath mappings for musescore files and metadata
+# Correct the filepath mappings for musescore files and metadata.
 
-# python ./remap_paths.py
+# python /home/pnlong/model_musescore/remap_metadata_to_data.py
 
 # IMPORTS
 ##################################################
+
 import glob
 from os.path import isfile, exists
 from tqdm import tqdm
@@ -15,33 +16,65 @@ import multiprocessing
 import json
 import pandas as pd
 import logging
-##################################################
+
+from dataset_full import MUSESCORE_DIR, INPUT_DIR, CHUNK_SIZE
+import utils
+
+############################################### ###
 
 
 # CONSTANTS
 ##################################################
-MSCZ_DIR = "/data2/zachary/musescore"
-ORIGINAL_FILEPATH = f"{MSCZ_DIR}/metadata2path.json"
-OUTPUT_FILEPATH = "/data2/pnlong/musescore/metadata_to_data.csv"
+
+OUTPUT_FILEPATH = f"{INPUT_DIR}/metadata_to_data.csv"
 MAPPINGS_COLUMNS = ["metadata", "data", "metadata_path", "data_path"]
+
 ##################################################
 
 
+# ARGUMENTS
+##################################################
+
+def parse_args(args = None, namespace = None):
+    """Parse command-line arguments."""
+    parser = argparse.ArgumentParser(prog = "Remap Metadata to Data", description = "Create data table mapping metadata to data files.")
+    parser.add_argument("-m", "--musescore_dir", type = str, default = MUSESCORE_DIR, help = "Directory where MuseScore data and metadata is stored")
+    parser.add_argument("-o", "--output_filepath", type = str, default = OUTPUT_FILEPATH, help = "Output filepath")
+    parser.add_argument("-j", "--jobs", type = int, default = int(multiprocessing.cpu_count() / 4), help = "Number of Jobs")
+    return parser.parse_args(args = args, namespace = namespace)
+
+##################################################
+
+
+# MAIN METHOD
+##################################################
+
 if __name__ == "__main__":
 
-    logging.basicConfig(level = logging.INFO, format = "%(message)s")
+    if not exists(args.output_filepath):
 
-    if not exists(OUTPUT_FILEPATH):
+        # SET UP
+        ##################################################
+
+        # parse arguments
+        args = parse_args()
+
+        # set up logging
+        logging.basicConfig(level = logging.INFO, format = "%(message)s")
+
+        ##################################################
+
 
         # GET FILEPATHS WITH GLOB + WRONG MAPPINGS
         ##################################################
 
-        metadata_paths = glob.iglob(pathname = f"{MSCZ_DIR}/metadata/**", recursive = True) # glob filepaths recursively, generating an iterator object
+        # create dictionary
+        metadata_paths = glob.iglob(pathname = f"{args.musescore_dir}/metadata/**", recursive = True) # glob filepaths recursively, generating an iterator object
         metadata_paths = (metadata_path for metadata_path in metadata_paths if isfile(metadata_path)) # filter out non-file elements that were globbed
         metadata_paths = {metadata_path.split("/")[-1].split(".")[0] : metadata_path for metadata_path in metadata_paths} # turn into dictionary, will make finding files much faster
 
         # import mappings
-        with open(ORIGINAL_FILEPATH, "r") as file:
+        with open(f"{args.musescore_dir}/metadata2path.json", "r") as file:
             mappings = json.load(fp = file)
         mappings = pd.DataFrame.from_dict(data = mappings, orient = "index").reset_index(drop = False).rename(columns = {"index": "metadata", 0: "data_path"})
         mappings["data"] = mappings["data_path"].apply(lambda data_path: data_path.split("/")[-1].split(".")[0])
@@ -55,7 +88,7 @@ if __name__ == "__main__":
 
         # create remappings dataframe
         remappings = pd.DataFrame(columns = MAPPINGS_COLUMNS) # create dataframe
-        remappings.to_csv(path_or_buf = OUTPUT_FILEPATH, sep = ",", na_rep = "NA", header = True, index = False, mode = "w")
+        remappings.to_csv(path_or_buf = args.output_filepath, sep = ",", na_rep = utils.NA_STRING, header = True, index = False, mode = "w")
 
         def find_metadata_path(i: int):
 
@@ -70,11 +103,11 @@ if __name__ == "__main__":
 
             # add metadata path to list to output
             current = current.to_list() # convert to list
-            current.insert(2, metadata_path)# add metadatapath to current
+            current.insert(2, metadata_path) # add metadatapath to current
 
             # output current
             remappings_current = pd.DataFrame(data = [dict(zip(MAPPINGS_COLUMNS, current))], columns = MAPPINGS_COLUMNS)
-            remappings_current.to_csv(path_or_buf = OUTPUT_FILEPATH, sep = ",", na_rep = "NA", header = False, index = False, mode = "a")
+            remappings_current.to_csv(path_or_buf = args.output_filepath, sep = ",", na_rep = utils.NA_STRING, header = False, index = False, mode = "a")
 
     ##################################################
 
@@ -83,11 +116,14 @@ if __name__ == "__main__":
     ##################################################
 
     # run with multiprocessing
-    chunk_size = 1
-    with multiprocessing.Pool(processes = int(multiprocessing.cpu_count() / 4)) as pool:
-        results = list(tqdm(iterable = pool.imap_unordered(func = find_metadata_path, iterable = range(len(mappings)), chunksize = chunk_size),
-                            desc = "Finding filepaths",
-                            total = len(mappings)))
+    with multiprocessing.Pool(processes = args.jobs) as pool:
+        results = list(tqdm(iterable = pool.imap_unordered(
+                func = find_metadata_path,
+                iterable = range(len(mappings)),
+                chunksize = CHUNK_SIZE,
+            ),
+            desc = "Finding Filepaths",
+            total = len(mappings)))
 
     ##################################################
 
@@ -96,12 +132,13 @@ if __name__ == "__main__":
     ##################################################
 
     # load in remappings
-    remappings = pd.read_csv(filepath_or_buffer = OUTPUT_FILEPATH, sep = ",", header = 0, index_col = False)
-
-    logging.info("STATISTICS:")
+    remappings = pd.read_csv(filepath_or_buffer = args.output_filepath, sep = ",", header = 0, index_col = False)
 
     # metadata not found
+    logging.info("STATISTICS:")
     n_metadata_not_found = len(remappings[pd.isna(remappings["metadata_path"])])
     logging.info(f"{n_metadata_not_found} ({100 * (n_metadata_not_found / len(remappings)):.2f}%) of metadata files could not be located.")
 
     ##################################################
+
+##################################################
