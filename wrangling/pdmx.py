a6868d50808d2d6a88ad5b0e50ad9a80a2f537ee
pnlong <phillipnlong@gmail.com>
2025-01-14 00:55:55 -0800

    updated musescore cli calls to be more efficient with batching

--- wrangling/pdmx.py
+++ wrangling/pdmx.py
@@ -18,7 +18,8 @@ import pandas as pd
 from tqdm import tqdm
 import multiprocessing
 import logging
-from typing import Tuple
+import tempfile
+import json
 
 from os.path import dirname, realpath
 import sys
@@ -30,7 +31,9 @@ from full import OUTPUT_DIR as DATASET_OUTPUT_DIR
 from reading.read_musescore import read_musescore
 import utils
 
-environ["QT_QPA_PLATFORM"] = "offscreen" # for musescore cli
+# environ["QT_QPA_PLATFORM"] = "offscreen" # for musescore 4 cli
+environ["QT_QPA_PLATFORM"] = "xcb" # for musescore 3 cli
+environ["DISPLAY"] = ":1" # for musescore 3 cli
 
 ##################################################
 
@@ -42,7 +45,7 @@ environ["QT_QPA_PLATFORM"] = "offscreen" # for musescore cli
 OUTPUT_DIR = "/data1/pnlong/musescore"
 
 # path to MuseScore CLI application
-MUSESCORE_APPLICATION_PATH = "/data3/pnlong/MuseScore-Studio-4.4.4.243461245-x86_64.AppImage" # MuseScore Command Line Options: https://musescore.org/en/handbook/3/command-line-options
+MUSESCORE_APPLICATION_PATH = "/data3/pnlong/MuseScore-3.6.2.548021370-x86_64.AppImage" # MuseScore Command Line Options: https://musescore.org/en/handbook/3/command-line-options
 
 # name of dataset
 DATASET_NAME = "PDMX"
@@ -125,6 +128,7 @@ if __name__ == "__main__":
     dataset["metadata_output"] = list(map(lambda path: metadata_dir + path[len(f"{MUSESCORE_DIR}/metadata"):] if (not pd.isna(path)) else None, dataset["metadata"]))
     dataset["mxl_output"] = list(map(lambda path: mxl_dir + path[len(data_dir):-len(".json")] + ".mxl", dataset["path_output"]))
     dataset["pdf_output"] = list(map(lambda path: pdf_dir + path[len(data_dir):-len(".json")] + ".pdf", dataset["path_output"]))
+    dataset[LICENSE_DISCREPANCY_COLUMN_NAME] = [False] * len(dataset) # create license discrepancy column
 
     # create necessary directory trees if required
     for column in output_columns:
@@ -135,27 +139,27 @@ if __name__ == "__main__":
     ##################################################
 
 
-    # COPY OVER FILES
+    # GENERATE MusicRender JSON FILES
     ##################################################
 
     # helper function to save files
-    def get_file(i: int) -> Tuple[str, str, str, str, bool]:
+    def get_json_metadata(i: int):
         """
-        Given the dataset index, copy over song as music object, and metadata, while generating other files.
-        Returns tuple of JSONified data path, metadata path, MusicXML path, PDF path.
+        Given the dataset index, copy over song as music object and metadata.
+        Edits the `dataset` DataFrame in place.
         """
 
-        # musescore path
+        # musescore file path
         path = dataset.at[i, "path"]
 
         # save as music object
         path_output = dataset.at[i, "path_output"]
         music = read_musescore(path = path, timeout = 10)
-        license_discrepancy = music.metadata.copyright is not None # if the metadata says a song has a public domain license, but the internals of the MuseScore file say elsewise
+        dataset.at[i, LICENSE_DISCREPANCY_COLUMN_NAME] = music.metadata.copyright is not None # if the metadata says a song has a public domain license, but the internals of the MuseScore file say elsewise
         if not exists(path_output) or args.reset: # save if necessary
             music.save(path = path_output, compressed = COMPRESS_JSON_MUSIC_FILES) # save as music object
         del music
-        path_output = "." + path_output[len(output_dir):]
+        dataset.at[i, "path_output"] = "." + path_output[len(output_dir):]
 
         # copy over metadata path
         metadata_path = dataset.at[i, "metadata"]
@@ -163,62 +167,142 @@ if __name__ == "__main__":
         if metadata_path is not None and (not exists(metadata_path_output) or args.reset):
             copy(src = metadata_path, dst = metadata_path_output) # copy over metadata
         if metadata_path_output is not None:
-            metadata_path_output = "." + metadata_path_output[len(output_dir):]
-
-        # generate music xml file
-        mxl_path_output = dataset.at[i, "mxl_output"]
-        if not exists(mxl_path_output) or args.reset:
-            subprocess.run(
-                args = [args.musescore_filepath, "--export-to", mxl_path_output, path],
-                check = True,
-                stdout = subprocess.DEVNULL,
-                stderr = subprocess.DEVNULL,
-            )
-        mxl_path_output = "." + mxl_path_output[len(output_dir):]
-
-        # generate pdf file
-        pdf_path_output = dataset.at[i, "pdf_output"]
-        if not exists(pdf_path_output) or args.reset:
-            subprocess.run(
-                args = [args.musescore_filepath, "--export-to", pdf_path_output, path],
-                check = True,
-                stdout = subprocess.DEVNULL,
-                stderr = subprocess.DEVNULL,
-            )
-        pdf_path_output = "." + pdf_path_output[len(output_dir):]
-
-        # return paths
-        return (path_output, metadata_path_output, mxl_path_output, pdf_path_output, license_discrepancy)
+            dataset.at[i, "metadata_output"] = "." + metadata_path_output[len(output_dir):]
 
     # use multiprocessing
     with multiprocessing.Pool(processes = args.jobs) as pool:
-        dataset["path"], dataset["metadata"], dataset["mxl"], dataset["pdf"], dataset[LICENSE_DISCREPANCY_COLUMN_NAME] = list(zip(*list(pool.map(
-            func = get_file,
-            iterable = tqdm(iterable = dataset.index, desc = f"Creating {DATASET_NAME}", total = len(dataset)),
-            chunksize = CHUNK_SIZE)
-        )))
+        _ = list(tqdm(iterable = pool.imap_unordered(func = get_json_metadata, iterable = dataset.index, chunksize = CHUNK_SIZE),
+                      desc = "Generating JSON Files and Copying Metadata",
+                      total = len(dataset)))
+        
+
+    ##################################################
+
+
+    # GET MUSICXML AND PDF FILES
+    ##################################################
+
+    # helper function to save files
+    # batch_size = 10 # number of files to include in a batch
+    # def get_mxl_pdf(i: int):
+    #     """
+    #     Given a starting dataset index, generate MusicXML and PDF files with the MuseScore CLI.
+    #     Utilizes the MuseScore CLI batch jobs functionality, with `batch_size` defined above.
+    #     Edits the `dataset` DataFrame in place.
+    #     """
+
+    #     # get input and output paths for batch jobs
+    #     indicies = range(i, min(i + batch_size, len(dataset)))
+    #     paths_input = list(map(lambda j: dataset.at[j, "path"], indicies)) # input musescore paths
+    #     paths_output = list(map(lambda j: [dataset.at[j, "mxl_output"], dataset.at[j, "pdf_output"]], indicies)) # output mxl and pdf paths
+
+    #     # get json dict object
+    #     json_dict = []
+    #     for path, (mxl_path_output, pdf_path_output) in zip(paths_input, paths_output):
+    #         song_dict = {"in": path, "out": []}
+    #         if not exists(mxl_path_output) or args.reset: # mxl
+    #             song_dict["out"].append(mxl_path_output)
+    #         if not exists(pdf_path_output) or args.reset: # pdf
+    #             song_dict["out"].append(pdf_path_output)
+    #         if len(song_dict["out"]) > 0: # append if there are some files to output
+    #             json_dict.append(song_dict)
+
+    #     # avoid unnecessary calculations
+    #     if len(json_dict) > 0:
+
+    #         # create a temporary directory
+    #         with tempfile.TemporaryDirectory() as temp_dir:
+
+    #             # write json dict to a json file
+    #             json_path = f"{temp_dir}/job.json"
+    #             with open(json_path, "w") as json_file:
+    #                 json.dump(json_dict, json_file)
+                
+    #             # run batch job with MuseScore CLI
+    #             subprocess.run(
+    #                 args = [args.musescore_filepath, "--job", json_path],
+    #                 check = True,
+    #                 stdout = subprocess.DEVNULL,
+    #                 stderr = subprocess.DEVNULL,
+    #             )
+
+    #     # update paths in `dataset` DataFrame
+    #     for j, (mxl_path_output, pdf_path_output) in zip(indicies, paths_output):
+    #         dataset.at[j, "mxl_output"] = "." + mxl_path_output[len(output_dir):]
+    #         dataset.at[j, "pdf_output"] = "." + pdf_path_output[len(output_dir):]
+
+    def get_mxl_pdf(i: int):
+        """
+        Given a starting dataset index, generate MusicXML and PDF files with the MuseScore CLI.
+        Edits the `dataset` DataFrame in place.
+        """
+
+        # create json dict object
+        json_dict = {"in": dataset.at[i, "path"], "out": []}
+        mxl_path_output = dataset.at[i, "mxl_output"]
+        pdf_path_output = dataset.at[i, "pdf_output"]
+        if not exists(mxl_path_output) or args.reset: # mxl
+            json_dict["out"].append(mxl_path_output)
+        if not exists(pdf_path_output) or args.reset: # pdf
+            json_dict["out"].append(pdf_path_output)
+
+        # avoid unnecessary calculations
+        if len(json_dict["out"]) > 0:
+
+            # create a temporary directory
+            with tempfile.TemporaryDirectory() as temp_dir:
+
+                # write json dict to a json file
+                json_path = f"{temp_dir}/job.json"
+                with open(json_path, "w") as json_file:
+                    json.dump([json_dict], json_file)
+                
+                # run batch job with MuseScore CLI, add except clause for corrupted files
+                try:
+                    subprocess.run(
+                        args = [args.musescore_filepath, "--job", json_path],
+                        check = True,
+                        stdout = subprocess.DEVNULL,
+                        stderr = subprocess.DEVNULL,
+                    )
+                except (subprocess.CalledProcessError):
+                    dataset.at[i, "mxl_output"] = None
+                    dataset.at[i, "pdf_output"] = None
+                    return
+        
+        # update paths in `dataset` DataFrame
+        dataset.at[i, "mxl_output"] = "." + mxl_path_output[len(output_dir):]
+        dataset.at[i, "pdf_output"] = "." + pdf_path_output[len(output_dir):]
+        
+    # use multiprocessing
+    with multiprocessing.Pool(processes = args.jobs) as pool:
+        _ = list(tqdm(iterable = pool.imap_unordered(func = get_mxl_pdf, iterable = dataset.index, chunksize = CHUNK_SIZE), 
+                      desc = "Generating MusicXML and PDF Files",
+                      total = len(dataset)))
 
     ##################################################
 
 
     # DO SOME WRANGLING TO MAKE DATASET NICER
     ##################################################
+
+    # add license discrepancy free facet
+    dataset[f"facet:no_{LICENSE_DISCREPANCY_COLUMN_NAME}"] = ~dataset[LICENSE_DISCREPANCY_COLUMN_NAME]
+    dataset["facet:valid_mxl_pdf"] = (~pd.isna(dataset["mxl_output"]) & ~pd.isna(dataset["pdf_output"]))
                                                         
-    # remove unnecessary columns
+    # rename facet columns
     dataset = dataset.drop(columns = output_columns)
     facet_columns = list(filter(lambda column: column.startswith("facet:"), dataset.columns))
-    subset_columns_prefix = "subset"
-    subset_columns = list(map(lambda facet_column: facet_column.replace("facet", subset_columns_prefix), facet_columns))
+    subset_columns = list(map(lambda facet_column: facet_column.replace("facet", "subset"), facet_columns))
     dataset = dataset.rename(columns = dict(zip(facet_columns, subset_columns))) # rename facet to subset columns
 
-    # add license conflict free subset
-    no_license_discrepancy_subset_name = f"{subset_columns_prefix}:no_{LICENSE_DISCREPANCY_COLUMN_NAME}"
-    subset_columns.append(no_license_discrepancy_subset_name)
-    dataset[no_license_discrepancy_subset_name] = ~dataset[LICENSE_DISCREPANCY_COLUMN_NAME]
+    # rename columns that are filepaths
+    dataset = dataset.drop(columns = ["path", "metadata"])
+    output_columns_new = list(map(lambda column: column.split("_")[0], output_columns))
+    dataset = dataset.rename(columns = dict(zip(output_columns, output_columns_new))) # rename output columns
 
     # reorder columns
-    dataset_columns = list(filter(lambda column: column not in subset_columns, dataset.columns)) + subset_columns # place subset columns at end
-    dataset_columns.remove(LICENSE_DISCREPANCY_COLUMN_NAME) # remove license discrepancy column
+    dataset_columns = output_columns_new + list(filter(lambda column: (column not in subset_columns) and (column not in output_columns_new) and column != LICENSE_DISCREPANCY_COLUMN_NAME, dataset.columns)) + subset_columns # place subset columns at end
     dataset_columns.insert(dataset_columns.index("license_url") + 1, LICENSE_DISCREPANCY_COLUMN_NAME) # add license discrepancy column back in
     dataset = dataset[dataset_columns] # reorder columns
 
