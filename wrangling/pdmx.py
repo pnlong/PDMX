a516bc71858923a6d5b840f824c5c789cdb27740
pnlong <phillipnlong@gmail.com>
2025-01-15 12:31:50 -0800

    fixed bug about editing dataset in place

--- wrangling/pdmx.py
+++ wrangling/pdmx.py
@@ -20,6 +20,7 @@ import multiprocessing
 import logging
 import tempfile
 import json
+from typing import Tuple
 
 from os.path import dirname, realpath
 import sys
@@ -143,10 +144,11 @@ if __name__ == "__main__":
     ##################################################
 
     # helper function to save files
-    def get_json_metadata(i: int):
+    def get_json_metadata(i: int) -> Tuple[str, str, bool]:
         """
         Given the dataset index, copy over song as music object and metadata.
-        Edits the `dataset` DataFrame in place.
+        Returns the output path, metadata output path, and whether the song 
+        has an internal license discrepancy.
         """
 
         # musescore file path
@@ -154,12 +156,12 @@ if __name__ == "__main__":
 
         # save as music object
         path_output = dataset.at[i, "path_output"]
-        music = read_musescore(path = path, timeout = 10)
-        dataset.at[i, LICENSE_DISCREPANCY_COLUMN_NAME] = music.metadata.copyright is not None # if the metadata says a song has a public domain license, but the internals of the MuseScore file say elsewise
+        music = read_musescore(path = path, timeout = 20)
+        has_license_discrepancy = (music.metadata.copyright is not None) # if the metadata says a song has a public domain license, but the internals of the MuseScore file say elsewise
         if not exists(path_output) or args.reset: # save if necessary
             music.save(path = path_output, compressed = COMPRESS_JSON_MUSIC_FILES) # save as music object
         del music
-        dataset.at[i, "path_output"] = "." + path_output[len(output_dir):]
+        path_output = "." + path_output[len(output_dir):]
 
         # copy over metadata path
         metadata_path = dataset.at[i, "metadata"]
@@ -167,15 +169,21 @@ if __name__ == "__main__":
         if metadata_path is not None and (not exists(metadata_path_output) or args.reset):
             copy(src = metadata_path, dst = metadata_path_output) # copy over metadata
         if metadata_path_output is not None:
-            dataset.at[i, "metadata_output"] = "." + metadata_path_output[len(output_dir):]
+            metadata_path_output = "." + metadata_path_output[len(output_dir):]
+
+        # return values
+        return (path_output, metadata_path_output, has_license_discrepancy)
 
     # use multiprocessing
     with multiprocessing.Pool(processes = args.jobs) as pool:
-        _ = list(tqdm(iterable = pool.imap_unordered(func = get_json_metadata, iterable = dataset.index, chunksize = CHUNK_SIZE),
-                      desc = "Generating JSON Files and Copying Metadata",
-                      total = len(dataset)))
+        dataset["path_output"], dataset["metadata_output"], dataset[LICENSE_DISCREPANCY_COLUMN_NAME] = list(zip(*list(
+            pool.map(func = get_json_metadata,
+                     iterable = tqdm(iterable = dataset.index,
+                                     desc = "Generating JSON Files and Copying Metadata",
+                                     total = len(dataset)),
+                     chunksize = CHUNK_SIZE)
+        )))
         
-
     ##################################################
 
 
@@ -184,11 +192,11 @@ if __name__ == "__main__":
 
     # helper function to save files
     # batch_size = 10 # number of files to include in a batch
-    # def get_mxl_pdf(i: int):
+    # def get_mxl_pdf(i: int) -> Tuple[str, str]:
     #     """
     #     Given a starting dataset index, generate MusicXML and PDF files with the MuseScore CLI.
     #     Utilizes the MuseScore CLI batch jobs functionality, with `batch_size` defined above.
-    #     Edits the `dataset` DataFrame in place.
+    #     Returns the output paths of the MXL and PDF files.
     #     """
 
     #     # get input and output paths for batch jobs
@@ -228,13 +236,17 @@ if __name__ == "__main__":
 
     #     # update paths in `dataset` DataFrame
     #     for j, (mxl_path_output, pdf_path_output) in zip(indicies, paths_output):
-    #         dataset.at[j, "mxl_output"] = "." + mxl_path_output[len(output_dir):]
-    #         dataset.at[j, "pdf_output"] = "." + pdf_path_output[len(output_dir):]
+    #         mxl_path_output = "." + mxl_path_output[len(output_dir):]
+    #         pdf_path_output = "." + pdf_path_output[len(output_dir):]
 
-    def get_mxl_pdf(i: int):
+    #     # return paths
+    #     return (mxl_path_output, pdf_path_output)
+
+    def get_mxl_pdf(i: int) -> Tuple[str, str]:
         """
-        Given a starting dataset index, generate MusicXML and PDF files with the MuseScore CLI.
-        Edits the `dataset` DataFrame in place.
+        Given a starting dataset index, generate MusicXML and PDF 
+        files with the MuseScore CLI.
+        Returns the output paths of the MXL and PDF files.
         """
 
         # create json dict object
@@ -266,19 +278,24 @@ if __name__ == "__main__":
                         timeout = 60, # wait for 60 seconds, if it's not done by then, then don't bother
                     )
                 except (subprocess.CalledProcessError, subprocess.TimeoutExpired): # if musescore file is corrupted
-                    dataset.at[i, "mxl_output"] = None
-                    dataset.at[i, "pdf_output"] = None
-                    return
+                    return (None, None)
         
-        # update paths in `dataset` DataFrame
-        dataset.at[i, "mxl_output"] = "." + mxl_path_output[len(output_dir):]
-        dataset.at[i, "pdf_output"] = "." + pdf_path_output[len(output_dir):]
+        # update paths
+        mxl_path_output = "." + mxl_path_output[len(output_dir):]
+        pdf_path_output = "." + pdf_path_output[len(output_dir):]
+
+        # return paths
+        return (mxl_path_output, pdf_path_output)
         
     # use multiprocessing
     with multiprocessing.Pool(processes = args.jobs) as pool:
-        _ = list(tqdm(iterable = pool.imap_unordered(func = get_mxl_pdf, iterable = dataset.index, chunksize = CHUNK_SIZE), 
-                      desc = "Generating MXL and PDF Files",
-                      total = len(dataset)))
+        dataset["mxl_output"], dataset["pdf_output"] = list(zip(*list(
+            pool.map(func = get_mxl_pdf,
+                     iterable = tqdm(iterable = dataset.index,
+                                     desc = "Generating MXL and PDF Files",
+                                     total = len(dataset)),
+                     chunksize = CHUNK_SIZE)
+        )))
 
     ##################################################
 
